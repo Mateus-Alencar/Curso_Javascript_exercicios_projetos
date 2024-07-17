2f4439d0a3f860b91ef87caac63cc7871c284576
Mateus-Alencar <mateus.alencar1@outlook.com>
2024-07-17 10:14:29 -0300

    first commit

---
+++ 2_TIPOS_DE_DADOS_E_OPERADORES/1_number/scripts.js
@@ -0,0 +1,8 @@
+console.log(typeof 5);
+console.log(typeof 12.7);
+console.log( -123);
+
+
+console.log(typeof 4);
+
+console.log(typeof "asfdasdf")
---
+++ 4_ESTRUTURAS_DE_PROGRAMACAO/12_while/scripts.js
@@ -0,0 +1,14 @@
+let x = 10;
+
+while(x > 0) {
+  console.log("O x é " + x);
+  x = x - 1;
+}
+
+let y = 0;
+
+while(y <= 10) {
+  console.log(y);
+  y = y + 1;
+}
+
